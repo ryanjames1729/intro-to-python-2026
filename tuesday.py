cc7b3cd2d03d9644ef6f5f257a4c1a217c303f0e
Ryan James <ryanjames1729@gmail.com>
2026-01-06 19:39:50 +0000

    updated changes at end of class

--- tuesday.py
+++ tuesday.py
@@ -1,3 +1,21 @@
 print()
 print("Happy New Year")
 print()
+
+schoolName = "CDS"
+print("Welcome to " + schoolName + ".") #concatenation
+
+#Example on input below
+yourName = input("Type your name:>")
+print("Nice to meet you, " + yourName)
+
+#Program for Today:
+#Place
+#Movie
+#Book
+#Fun Fact
+place = input("Where did you go?>")
+movie = input("What did you watch?>")
+book = input("What did you read?>")
+funFact = input("What's a fun fact about your break?>")
+print("Mr. James went to " + place + ". Mr. James watched the movie, " + movie + ". He read the book, " + book + ". Here's an interesting fact: " + funFact)
