9b8012f55797b6501ef31e318470e1dfe36cddd3
Ryan James <ryanjames1729@gmail.com>
2026-01-09 15:57:23 +0000

    thursdays assignment

---
+++ thursday.py
@@ -0,0 +1,17 @@
+# Create 4 variables
+intVar = -1
+floatVar = 3.14
+strVar1 = "hello"
+strVar2 = "world"
+
+print(intVar)
+print(floatVar)
+print(strVar1)
+print(strVar2)
+
+print(str(intVar) + " " + str(floatVar) + " " + " " + strVar1 + " " + strVar2)
+
+strVar1 = input()
+strVar2 = input()
+
+print(str(intVar) + " " + str(floatVar) + " " + " " + strVar1 + " " + strVar2)
